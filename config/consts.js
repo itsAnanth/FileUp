affcba987009776c44a4a630fcac841d48c75f4f
EJBEAN2op <danonymousarmy@gmail.com>
2022-02-19 15:29:08 +0530

    chore(shared): TTL config

--- config/consts.js
+++ config/consts.js
@@ -1,3 +1,5 @@
 const FILE_PATH = '/uploads'
+const TTL_INTERVAL = 30 * 60 * 1000 // 30 minutes;
+const TTL_FILE = 24 * 60 * 60 * 1000 // 24 hours;
 
-export { FILE_PATH };
+export { FILE_PATH, TTL_INTERVAL, TTL_FILE };
