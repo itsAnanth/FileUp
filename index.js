e7a0ce82e81fe4e772de106d1e0ac89456c578d2
EJBEAN2op <danonymousarmy@gmail.com>
2022-02-19 15:29:35 +0530

    chore(server): cleanup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import multer from 'multer';
 import express from 'express';
 import Server from './server.js';
 import cors from 'cors';
+import TTL from './memory/TTL.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 const includeMulter = multer().any();
@@ -15,3 +16,5 @@ app.use((req, res, next) => {
 app.use(express.static('public'));
 
 new Server(app, PORT, true);
+
+TTL();
