3b41ad688fbe976a4f5c84a959ebba7386535c41
EJBEAN2op <danonymousarmy@gmail.com>
2022-02-19 13:15:38 +0530

    fix(server): add multer middleware

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+import multer from 'multer';
 import express from 'express';
 import Server from './server.js';
 import cors from 'cors';
-
 const app = express();
 const PORT = process.env.PORT || 3000;
+const includeMulter = multer().any();
 
 app.use(cors());
+
+app.use((req, res, next) => {
+    includeMulter(req, res, next)
+});
 
 app.use(express.static('public'));
 
