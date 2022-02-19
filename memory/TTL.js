1a1f51527547354e7be5af8d64374f53de8784c9
EJBEAN2op <danonymousarmy@gmail.com>
2022-02-19 18:42:01 +0530

    fix(server): leak
    remove redunant file details in json

--- memory/TTL.js
+++ memory/TTL.js
@@ -6,7 +6,10 @@ function delete_ttl() {
     for (const [k, v] of Object.entries(read())) {
         if (Date.now() - v.time > TTL_FILE) {
             const file = `./uploads/${k}.mp4`;
-            if (!fs.existsSync(file)) continue;
+            if (!fs.existsSync(file)) {
+                _delete(k);
+                continue
+            };
             fs.unlink(file, (err) => {
                 if (err) return console.error(err);
                 _delete(k);
