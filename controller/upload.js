877d5aec40f6475702e663d3d9287de17e2d04b6
EJBEAN2op <danonymousarmy@gmail.com>
2022-02-19 13:06:10 +0530

    feat(server): file upload controller

--- controller/upload.js
+++ controller/upload.js
@@ -1,5 +1,11 @@
 import pkg from 'express';
+import multer from 'multer';
 import path from 'path';
+import config from '../config/config.js';
+import { write, writeFile } from '../memory/index.js';
+import IResponse from '../utils/Response.js';
+import uuid from 'uuid-random';
+import fs from 'fs';
 
 const { Response, Request } = pkg;
 /**
@@ -11,4 +17,24 @@ const __init__ = (req, res) => {
     res.sendFile(path.resolve(__dirname + '/../public/index.html'))
 }
 
-export { __init__ };
+const upload = (request, response) => {
+
+    const file = request.files[0];
+    const fileId = uuid();
+    const link = `http://${request.hostname}/video/${fileId}`;
+
+    IResponse.success({ link: link });
+
+    const data = {
+        name: file.originalname,
+        size: file.size,
+        encoding: file.encoding,
+        details: request.body.details ? request.body.details : ''
+    }
+
+    writeFile(fileId, file.buffer);
+
+}
+
+export { __init__, upload };
+
